3420ea658ba9d592d41562b05c228d46d8cd9cf5
Clément Leblanc <cl.clement.leblanc@gmail.com>
2025-12-11 11:56:33 +0100

    v0.0.4

--- src/oven_time/core.py
+++ src/oven_time/core.py
@@ -56,9 +56,9 @@ def get_price_window(
     date_str = start_local.strftime("%d/%m")
 
     text = (
-        f"🌱⚡ *Fenêtre bas carbone à venir:*"
-        f"🕒 De *{start_str}* à *{end_str}* (le {date_str})\n\n"
-        f"👉 Créneau idéal pour lancer les gros consommateur d'électricité."
+        f"🌱⚡ Fenêtre bas carbone à venir de"
+        f"🕒 *{start_str}* à *{end_str}* 🕒 (le {date_str})\n\n"
+        f"👉 Créneau idéal pour lancer les gros consommateur d'électricité"
     )
 
     return text
